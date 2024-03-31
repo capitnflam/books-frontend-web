294874c951e427daf47d040ae6bf5d5cf5d45712
capitnflam <capitnflam@gmail.com>
2024-03-31 18:40:40 +0200

    feat: improve pagination component (#4)

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -12,34 +12,45 @@ interface PaginationProps {
   readonly total: number
   readonly page: number
   readonly onChange: (page: number) => void
+  readonly shownAtStartCount?: number
+  readonly shownAtEndCount?: number
 }
 
 export function Pagination({
   total,
   page: inputPage,
   onChange,
+  shownAtStartCount = 2,
+  shownAtEndCount = 2,
 }: PaginationProps) {
   const page = inputPage <= 0 ? 1 : inputPage > total ? total : inputPage
+  const shownAtStart =
+    shownAtStartCount > 0 ? Math.min(shownAtStartCount, total) : 1
+  const shownAtEnd = shownAtEndCount > 0 ? Math.min(shownAtEndCount, total) : 1
 
   return (
     <PaginationShad>
       <PaginationContent>
         {page > 1 && (
-          <PaginationItem className="">
+          <PaginationItem>
             <PaginationPrevious onClick={() => onChange(page - 1)} />
           </PaginationItem>
         )}
-        {page > 2 && (
-          <PaginationItem>
-            <PaginationLink onClick={() => onChange(1)}>1</PaginationLink>
-          </PaginationItem>
+        {Array.from({ length: shownAtStart }, (_, index) => index + 1).map(
+          (index) => {
+            if (page <= index + 1) {
+              return
+            }
+            return (
+              <PaginationItem key={index}>
+                <PaginationLink onClick={() => onChange(index)}>
+                  {index}
+                </PaginationLink>
+              </PaginationItem>
+            )
+          },
         )}
-        {page > 3 && (
-          <PaginationItem>
-            <PaginationLink onClick={() => onChange(2)}>2</PaginationLink>
-          </PaginationItem>
-        )}
-        {page > 4 && (
+        {page > shownAtStart + 2 && (
           <PaginationItem>
             <PaginationEllipsis />
           </PaginationItem>
@@ -63,25 +74,26 @@ export function Pagination({
             </PaginationLink>
           </PaginationItem>
         )}
-        {page < total - 3 && (
+        {page < total - (shownAtEnd + 1) && (
           <PaginationItem>
             <PaginationEllipsis />
           </PaginationItem>
         )}
-        {page < total - 2 && (
-          <PaginationItem>
-            <PaginationLink onClick={() => onChange(total - 1)}>
-              {total - 1}
-            </PaginationLink>
-          </PaginationItem>
-        )}
-        {page < total - 1 && (
-          <PaginationItem>
-            <PaginationLink onClick={() => onChange(total)}>
-              {total}
-            </PaginationLink>
-          </PaginationItem>
-        )}
+        {Array.from(
+          { length: shownAtEnd },
+          (_, index) => total - shownAtEnd + index + 1,
+        ).map((index) => {
+          if (page >= index - 1) {
+            return
+          }
+          return (
+            <PaginationItem key={index}>
+              <PaginationLink onClick={() => onChange(index)}>
+                {index}
+              </PaginationLink>
+            </PaginationItem>
+          )
+        })}
         {page < total && (
           <PaginationItem>
             <PaginationNext onClick={() => onChange(page + 1)} />
